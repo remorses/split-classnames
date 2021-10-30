6b65543733a8ce9bf5a85dd5651000d0cf064b39
remorses <beats.by.morse@gmail.com>
2021-10-30 21:52:43 +0200

    codemod/src/examples/alreadyRun:

--- codemod/src/examples/alreadyRun.jsx
+++ codemod/src/examples/alreadyRun.jsx
@@ -1,23 +1,26 @@
 import React from 'react'
 
+import clsx from 'classnames'
+
 export default function Component() {
     return (
         <div>
             <p className='ciao'></p>
-            <p className={clsx('a b c d e f g')}></p>
+            <p className={clsx('a b c d e', 'f g')}></p>
             <p
                 className={clsx(
-                    'a b c d e f g',
+                    'a b c d e',
+                    'f g',
                     'another one',
                     true && 'another',
                 )}
             ></p>
-            <p className={`ciao sono un template literal ${x}`}></p>
-            <p className={`${cond ? 'literal-class' : ''}`}></p>
-            <p className={`x ${cond ? 'literal-class' : ''} z`}></p>
+            <p className={clsx('ciao sono un template literal', x)}></p>
+            <p className={clsx(cond ? 'literal-class' : '')}></p>
+            <p className={clsx('x', cond ? 'literal-class' : '', 'z')}></p>
 
-            <p className={'ciao sono un literal'}></p>
-            <p className='a b c d e f g'></p>
+            <p className={clsx('ciao sono un literal')}></p>
+            <p className={clsx('a b c d e', 'f g')}></p>
         </div>
     )
 }
