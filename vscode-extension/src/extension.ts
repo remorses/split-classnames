2e4a3c568ab6cc99fcc9f35160d3f41c57e4d0f6
remorses <beats.by.morse@gmail.com>
2021-10-31 13:17:26 +0100

    more features to extension

--- vscode-extension/src/extension.ts
+++ vscode-extension/src/extension.ts
@@ -1,20 +1,55 @@
 import * as vscode from 'vscode'
 
-export function activate(context: vscode.ExtensionContext) {
-    console.log(
-        'Congratulations, your extension "vscode-extension" is now active!',
-    )
+import { transformSource } from 'codemod-split-classnames'
+const config = vscode.workspace.getConfiguration()
 
-    let disposable = vscode.commands.registerCommand(
-        'vscode-extension.helloWorld',
-        () => {
-            vscode.window.showInformationMessage(
-                'Hello World from vscode-extension!',
+const SPLIT_CLASSNAMES_COMMAND = 'vscode-extension.splitClassNames'
+
+const allowedLanguageIds = new Set([
+    'typescript',
+    'typescriptreact',
+    'javascript',
+    'javascriptreact',
+])
+
+export function activate(context: vscode.ExtensionContext) {
+    console.log('Extension active')
+
+    let disposable1 = vscode.commands.registerTextEditorCommand(
+        SPLIT_CLASSNAMES_COMMAND,
+        function (editor, edit) {
+            const editorText = editor.document.getText()
+            const editorLangId = editor.document.languageId
+            if (!allowedLanguageIds.has(editorLangId)) {
+                vscode.window.showWarningMessage(
+                    `${editorLangId} is not supported by vscode-extension`,
+                )
+                return
+            }
+            const range = new vscode.Range(
+                editor.document.positionAt(0),
+                editor.document.positionAt(editorText.length),
             )
+            try {
+                const result = transformSource(editorText, {})
+                edit.replace(range, result)
+            } catch (e: any) {
+                vscode.window.showErrorMessage(
+                    `Error splitting classnames: ${e.message}`,
+                )
+            }
+            // TODO run prettier formatting if prettier config is found? Maybe try running prettier command from vscode?
         },
     )
+    context.subscriptions.push(disposable1)
 
-    context.subscriptions.push(disposable)
+    if (config.get('vscode-extension.runOnSave')) {
+        context.subscriptions.push(
+            vscode.workspace.onWillSaveTextDocument((_e) => {
+                vscode.commands.executeCommand(SPLIT_CLASSNAMES_COMMAND)
+            }),
+        )
+    }
 }
 
 export function deactivate() {}
