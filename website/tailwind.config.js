e2fdebc82e1624c8aae8afa23239435a8495aff3
remorses <beats.by.morse@gmail.com>
2021-11-01 10:09:43 +0100

    made website

---
+++ website/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss/tailwind-config').TailwindConfig} */
+module.exports = {
+    mode: 'jit',
+    purge: [
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
+    ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
