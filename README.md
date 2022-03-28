bf0c01a9d950ff4b2bdd2a9fee3daf6517d53f6d
remorses <beats.by.morse@gmail.com>
2022-03-28 13:16:35 +0200

    update readme

--- README.md
+++ README.md
@@ -30,10 +30,11 @@ function Component() {
         <Fragment>
             <p
                 className={clsx(
-                    'block w-full py-2 mt-8 text-sm',
-                    'font-semibold text-center text-white bg-gray-900 border',
-                    'border-black rounded-md hover:bg-gray-700 disabled:bg-gray-900 hover:cursor-pointer',
+                    'block w-full py-2 mt-8 text-sm font-semibold text-center',
+                    'text-white bg-gray-900 border border-black rounded-md',
+                    'disabled:bg-gray-900 hover:cursor-pointer',
                     'disabled:opacity-60',
+                    'hover:bg-gray-700',
                 )}
             />
         </Fragment>
--- split-classnames/example/output.jsx
+++ split-classnames/example/output.jsx
@@ -1,12 +1,14 @@
 import clsx from 'classnames'
-function Component(x) {
+function Component() {
     return (
         <Fragment>
             <p
                 className={clsx(
-                    'block w-full py-2 mt-8 text-sm font-semibold text-center text-white',
-                    'bg-gray-900 border border-black rounded-md w-[340px] disabled:opacity-60',
-                    'hover:cursor-pointer disabled:bg-gray-900 hover:bg-gray-700 sm:h-[60em]',
+                    'block w-full py-2 mt-8 text-sm font-semibold text-center',
+                    'text-white bg-gray-900 border border-black rounded-md',
+                    'disabled:bg-gray-900 hover:cursor-pointer',
+                    'disabled:opacity-60',
+                    'hover:bg-gray-700',
                 )}
             />
         </Fragment>
