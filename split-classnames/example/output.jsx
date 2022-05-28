c21e007ff3a47c871027e72a356f34f3bde7e4b0
remorses <beats.by.morse@gmail.com>
2022-05-28 14:57:57 +0200

    split-classnames/example/output:

--- split-classnames/example/output.jsx
+++ split-classnames/example/output.jsx
@@ -6,9 +6,8 @@ function Component() {
                 className={clsx(
                     'block w-full py-2 mt-8 text-sm font-semibold text-center',
                     'text-white bg-gray-900 border border-black rounded-md',
-                    'disabled:bg-gray-900 hover:cursor-pointer',
-                    'disabled:opacity-60',
-                    'hover:bg-gray-700',
+                    'hover:bg-gray-700 disabled:opacity-60 hover:cursor-pointer',
+                    'disabled:bg-gray-900',
                 )}
             />
         </Fragment>
